1ec6d732a363aa2b4ba63352f6d6b9095d6d8b08
najib <najib@najib-HP-Pavilion-Notebook>
2018-02-24 18:57:13 +0000

    j'ai ajouter une clase des etudiant et jai tester le commit

---
+++ projet/src/principale/Etudiant.java
@@ -0,0 +1,19 @@
+package principale;
+
+public class Etudiant {
+	private int id; 
+	private String login;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	} 
+	
+}
